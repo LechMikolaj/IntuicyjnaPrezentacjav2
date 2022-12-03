5572860dc88f6104715651e1090b6f742ea6833a
LechMikolaj <lech_mikolaj@interia.pl>
2022-12-03 15:47:15 +0100

    .

--- main.py
+++ main.py
@@ -3,77 +3,37 @@ from fps import fps
 from changingSlides import slides,countOfSlides
 from slidesCounter import slideCounter
 from poseDetection import poseDetection
-
-import mediapipe as mp
-
-
-
-
-
 handAtEnd=False
 handAtBeginning=False
-
-
-
-
-
-
-            # print("leftWrist_positionx:",leftWrist_positionx*h)
+       # print("leftWrist_positionx:",leftWrist_positionx*h)
             # print("hip_positionx:",hip_positionx*h)
         #dopisac rozpoznawanie odpowiedniej pozycji reki na podstawie results.pose_landmarks
         #pozycja reki jak do przesuwania w prawo slajdu
         #dopisac ostrzeżenie o zbyt malej odleglosci
-
-
-
-import time
-pTime=0.0
-import cv2
-#function showing fps on image
-
-
-slides =[]
-countOfSlides=5
-for i in range(1,countOfSlides+1):
-    slides.append(cv2.imread(f"slajd{i}.png"))
-
-
-
 DEBUG = True
-if_flip=False
+if_flip=True
 print_Hands=True
 i=1
 print(f"DEBUG:{DEBUG}")
 # fps dziala
-
 slideCounter(slides[0], i, countOfSlides) #first position of countOfSlides
 cv2.imshow("Slide",slides[0])#first slide
-
 cap=cv2.VideoCapture(0)
 #cap=cv2.VideoCapture("video-1646058581.mp4")
-
 while True:
     ret, img=cap.read()
-
     flipcode=1
-
     if if_flip:
         img=cv2.flip(img, flipcode)    #image from camera flip horizontally
-
-
     #handsDetecion(img) # function detecting hands from image flipped
     ifChangeSlide=poseDetection(img,DEBUG) # function detecting pose from image flipped
     if ifChangeSlide:
-
         i=i%(countOfSlides)
         slideCounter(slides[i], i+1, countOfSlides)
         cv2.imshow("Slide",slides[i])
         i=i+1
-
         if DEBUG:
             print(i)
-
-
     if DEBUG==True:
         fps(img) # function showing fps on image
     cv2.imshow("Image", img) #showing image
--- poseDetection.py
+++ poseDetection.py
@@ -1,6 +1,4 @@
 import cv2
-
-
 from pose import Pose
 handAtEnd=False
 handAtBeginning=False
@@ -11,35 +9,24 @@ def poseDetection(img,DEBUG):
     imgRGB=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     mpPose=Pose.pose
     mpDraw = Pose.mwDraw
-
-
-
     results=mpPose.process(imgRGB)
     h,w,c=img.shape
     id_leftWrist=15
     id_hip=23
-
     leftWrist_positionx=0
     hip_positionx = 0
-
-
     if results.pose_landmarks:
         if DEBUG:
             mpDraw.draw_landmarks(img,results.pose_landmarks,Pose.mpPose.POSE_CONNECTIONS)
-
         for id,landmark in enumerate(results.pose_landmarks.landmark):
             if id==id_leftWrist:
                 leftWrist_positionx =landmark.x
             if id==id_hip:
                 hip_positionx =landmark.x
-
-
         if leftWrist_positionx*h < hip_positionx*h:
             handAtEnd=True
-
         if leftWrist_positionx*h > hip_positionx*h :
             handAtBeginning=True
-
         if handAtEnd==True and handAtBeginning==True:
             handAtEnd=False
             handAtBeginning=False
@@ -47,7 +34,6 @@ def poseDetection(img,DEBUG):
                 return True
             else:
                 return False
-
             # print("leftWrist_positionx:",leftWrist_positionx*h)
             # print("hip_positionx:",hip_positionx*h)
         #dopisac rozpoznawanie odpowiedniej pozycji reki na podstawie results.pose_landmarks
